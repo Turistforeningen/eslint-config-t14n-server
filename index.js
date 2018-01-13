27a00471e10946e5c45749fa6aca63b46cf91a47
Roar Skaar <roar.skaar@gmail.com>
2018-01-13 07:05:03 +0100

    chore(project): initialize

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'script',
+    ecmaFeatures: {
+      modules: false
+    }
+  },
+  rules: {
+    'no-console': [0],
+    'no-param-reassign': [2, {'props': false}],
+    'no-underscore-dangle': [2, {'allow': ['_id']}],
+    'no-unused-vars': [2, {'args': 'none'}],
+    'function-paren-newline': [2, 'consistent'],
+    'arrow-parens': [2, 'always'],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'ignore'
+    }]
+  }
+};
